45b3fe562e2d477990b9f3af2c301ca5d63c1f69
sheanWinston <shenwinston@gmail.com>
2021-01-26 09:05:56 +0100

    lessons data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,154 +1,90 @@
 <template>
   <div class="home">
-        <section class="banner banner-2">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-12 col-lg-8">
-                        <div class="banner-content center-heading">
-                            <span class="subheading">Expert instruction</span>
-                            <h1>Build Skills With Experts Any Time, Anywhere </h1>
-                            <p>We invest in personnel, technological innovations and infrastructure and have established
-                                regional and international offices.</p>
-                            <a href="#" class="btn btn-main"><i class="fa fa-list-ul mr-2"></i>our Courses </a>
-                        </div>
-                    </div>
-                </div> 
-            </div> 
-        </section>
-
-        <section class="section-padding popular-course pb-0">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div class="section-heading">
-                            <span class="subheading">Top Trending Courses</span>
-                            <h3>Our Popular Online Courses</h3>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-6">
-                        <div class="course-btn text-lg-right"><a href="#" class="btn btn-main"><i
-                                    class="fa fa-store mr-2"></i>All Courses</a></div>
-                    </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-md-4 col-5">
-                    <h3>Sort</h3>
-                  </div>
-                  <div class="col-md-8 col-7">
-                    <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="course-block">
-                            <div class="course-img">
-                                <img src="@/assets/images/course/course1.jpg" alt="" class="img-fluid">
-                                <span class="course-label">Beginner</span>
-                            </div>
-
-                            <div class="course-content">
-                                <div class="course-price ">$50</div>
-
-                                <h4><a href="#">Information About UI/UX Design Degree</a></h4>
-                                <div class="rating">
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <span>(5.00)</span>
-                                </div>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
-
-                                <div class="course-footer d-lg-flex align-items-center justify-content-between">
-                                    <div class="course-meta">
-                                        <span class="course-student"><i class="bi bi-group"></i>340</span>
-                                        <span class="course-duration"><i class="bi bi-badge3"></i>82 Lessons</span>
-                                    </div>
-
-                                    <div class="buy-btn"><a href="#" class="btn btn-main-2 btn-small">Details</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="course-block">
-                            <div class="course-img">
-                                <img src="@/assets/images/course/course2.jpg" alt="" class="img-fluid">
-                                <span class="course-label">Advanced</span>
-                            </div>
-
-                            <div class="course-content">
-                                <div class="course-price ">$80 <span class="del">$120</span></div>
-
-                                <h4><a href="#">Photography Crash Course for Photographer</a></h4>
-                                <div class="rating">
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <span>(5.00)</span>
-                                </div>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
-
-                                <div class="course-footer d-lg-flex align-items-center justify-content-between">
-                                    <div class="course-meta">
-                                        <span class="course-student"><i class="bi bi-group"></i>340</span>
-                                        <span class="course-duration"><i class="bi bi-badge3"></i>82 Lessons</span>
-                                    </div>
-
-                                    <div class="buy-btn"><a href="#" class="btn btn-main-2 btn-small">Details</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="course-block">
-                            <div class="course-img">
-                                <img src="@/assets/images/course/course3.jpg" alt="" class="img-fluid">
-                                <span class="course-label">Expert</span>
-                            </div>
-
-                            <div class="course-content">
-                                <div class="course-price ">$100 <span class="del">$180</span></div>
-
-                                <h4><a href="#">React – The Complete Guide (React Router)</a></h4>
-                                <div class="rating">
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <a href="#"><i class="fa fa-star"></i></a>
-                                    <span>(5.00)</span>
-                                </div>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, alias.</p>
-
-                                <div class="course-footer d-lg-flex align-items-center justify-content-between">
-                                    <div class="course-meta">
-                                        <span class="course-student"><i class="bi bi-group"></i>340</span>
-                                        <span class="course-duration"><i class="bi bi-badge3"></i>82 Lessons</span>
-                                    </div>
-
-                                    <div class="buy-btn"><a href="#" class="btn btn-main-2 btn-small">Details</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                  </div>
-                </div>
+    <section class="banner banner-2">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12 col-lg-8">
+            <div class="banner-content center-heading">
+              <span class="subheading">Expert instruction</span>
+              <h1>Build Skills With Experts Any Time, Anywhere </h1>
+              <p>We invest in personnel, technological innovations and infrastructure and have established
+                regional and international offices.</p>
+              <a href="#" class="btn btn-main"><i class="fa fa-list-ul mr-2"></i>our Courses </a>
             </div>
-        </section>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-padding popular-course pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <div class="section-heading">
+              <span class="subheading">Top Trending Courses</span>
+              <h3>Our Popular Online Courses</h3>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="course-btn text-lg-right"><a href="#" class="btn btn-main"><i class="fa fa-store mr-2"></i>All
+                Courses</a></div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3 col-5">
+            <h3>Sort</h3>
+          </div>
+          <div class="col-md-9 col-7">
+            <div class="row">
+              <div class="col-lg-4 col-md-6" v-for="lesson in lessons" :key="lesson.id">
+                <div class="course-block">
+                  <div class="course-img">
+                    <img :src="require(`@/assets/images/course/${lesson.image}`)" alt="" class="img-fluid">
+                  </div>
+
+                  <div class="course-content">
+                    <div class="course-price ">$50</div>
+
+                    <h5><a href="#">{{ lesson.subject }}</a></h5>
+                    <div class="rating">
+                      <i class="fa fa-map"></i>
+                      <span>{{ lesson.location }}</span>
+                    </div>
+                    <p>{{ lesson.description }}</p>
+
+                    <div class="course-footer d-lg-flex align-items-center justify-content-between">
+                      <div class="course-meta">
+                        <span class="course-student"><i class="bi bi-group"></i>{{ lesson.spaces }}</span>
+                      </div>
+
+                      <div class="buy-btn"><a href="#" class="btn btn-main-2 btn-small">Add to cart</a></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+  // @ is an alias to /src
+  import lessonsData from "../data/lessons.json";
 
-export default {
-  name: 'Home',
-  components: {
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        lessons: lessonsData
+      }
+    },
+    components: {
 
-}
-}
+    }
+  }
 </script>
