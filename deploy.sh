95a924227aa535529e7e29014e5f560e6c75e09a
Zakaria5156 <Zakaria5156@gmail.com>
2021-01-27 12:41:40 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 #git init
 git add -A
 git commit -m 'deploy'
-git push git@github.com:Zakaria5156/lessons-app.git master:gh-pages
+git push -f git@github.com:Zakaria5156/lessons-app.git master:gh-pages
 cd -
